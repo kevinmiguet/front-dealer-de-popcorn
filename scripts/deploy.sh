80fd9e108382e6de33b41e6bc794e252d1408943
kiki <kevin.miguet@gmx.fr>
2019-06-09 12:14:31 +0000

    fix(scripts): deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$PROD_DIR=/var/www/html/ugc/test09101991/dist
-$DEV_DIR=/var/www/html/repos/front-dealer-de-popcorn
+PROD_DIR=/var/www/html/ugc/test09101991/
+DEV_DIR=/var/www/html/repos/front-dealer-de-popcorn
 
 # Clean previous production directory
 rm -rf $PROD_DIR
 # Build new version of front
 cd $DEV_DIR && parcel build index.html --public-url ./ -d $PROD_DIR
 # copy export folder
-cp $DEV_DIR/export $PROD_DIR/export
+cp -r $DEV_DIR/export $PROD_DIR/export
