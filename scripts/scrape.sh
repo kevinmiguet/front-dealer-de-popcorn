9a65468abbc34f281dd0a6ee6b5fd295b4ebce50
kiki <kevin.miguet@gmx.fr>
2019-06-13 17:12:53 +0000

    fix(script): again

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'toto'
+
 cd /var/www/html/repos/allocine-scraper
 rm -rf /var/www/html/repos/front-dealer-de-popcorn/export 
-node /var/www/html/repos/allocine-scraper/build/main.js
+tsc && node /var/www/html/repos/allocine-scraper/build/main.js
  cp -r /var/www/html/repos/allocine-scraper/export /var/www/html/repos/front-dealer-de-popcorn/export
