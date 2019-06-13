5cf56b6e1d7c83edf6b3e79ea8060be9dc5f1afe
kiki <kevin.miguet@gmx.fr>
2019-06-13 17:07:55 +0000

    fix(scripts): fix it

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo 'toto'
 cd /var/www/html/repos/allocine-scraper
+rm -rf /var/www/html/repos/front-dealer-de-popcorn/export 
 node /var/www/html/repos/allocine-scraper/build/main.js
-cp -r /var/www/html/repos/allocine-scraper/export /var/www/html/repos/front-dealaer-de-popcorn/export
+ cp -r /var/www/html/repos/allocine-scraper/export /var/www/html/repos/front-dealer-de-popcorn/export
